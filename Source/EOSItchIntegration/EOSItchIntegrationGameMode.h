351c296e6bc25626d7396215f14cfbc1462bca06
JiRath <garethaguilar@gmail.com>
2024-08-30 20:44:25 -0500

    Push initial files

---
+++ Source/EOSItchIntegration/EOSItchIntegrationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EOSItchIntegrationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEOSItchIntegrationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEOSItchIntegrationGameMode();
+};
+
+
+
